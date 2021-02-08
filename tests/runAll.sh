fb3516d7f6e90b27e594b8c2d2fecd22b577da23
Victor Adail Ferrer <victor.adail@gmail.com>
2021-02-08 01:34:30 +0000

    Run all test

--- tests/runAll.sh
+++ tests/runAll.sh
@@ -2,16 +2,21 @@
 echo -e "Run All Test"
 
 echo -e "\n============================================================ Cyclomatic Complexity Test =========================================================== \n"
+chmod +x tests/complexity/test.sh
 tests/complexity/test.sh
 
 echo -e "\n==================================================================== Unit Test ==================================================================== \n"
+chmod +x tests/unit -v
 pytest tests/unit -v
 
 echo -e "\n=============================================================== Code Coverage Test ================================================================ \n"
+chmod +x tests/coverage/test.sh
 tests/coverage/test.sh
 
 echo -e "\n================================================================ Code Quality Test ================================================================ \n"
+chmod +x tests/quality/test.sh
 tests/quality/test.sh
 
 echo -e "\n=================================================================  Security Test ================================================================== \n"
+chmod +x tests/security/test.sh
 tests/security/test.sh
